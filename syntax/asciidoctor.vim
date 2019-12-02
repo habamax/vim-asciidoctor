habamax/vim-asciidoctor

commit d3ba853aa9a0dcc8fa4bd31359c9431db1c1d6e9

Support setext headers (#31) (#36)
Support setext headers (#31)

syntax/asciidoctor.vim
@@ -53,6 +53,12 @@ syn region asciidoctorH4 start="^=====\s" end="$" oneline contains=@asciidoctorI
 syn region asciidoctorH5 start="^======\s" end="$" oneline contains=@asciidoctorInline,@Spell
 syn region asciidoctorH6 start="^=======\s" end="$" oneline contains=@asciidoctorInline,@Spell
 
+syn match asciidoctorH1 '^\%(\n\|\%^\)\k\+.*\n==\+\n$'   contains=@Spell
+syn match asciidoctorH2 '^\%(\n\|\%^\)\k\+.*\n--\+\n$'   contains=@Spell
+syn match asciidoctorH3 '^\%(\n\|\%^\)\k\+.*\n\~\~\+\n$' contains=@Spell
+syn match asciidoctorH4 '^\%(\n\|\%^\)\k\+.*\n\^\^\+\n$' contains=@Spell
+syn match asciidoctorH5 '^\%(\n\|\%^\)\k\+.*\n++\+\n$'   contains=@Spell
+
 syn sync clear
 syn sync match syncH1 grouphere NONE "^==\s.*$"
 syn sync match syncH2 grouphere NONE "^===\s.*$"

commit 0f093ee9a9026abde317cc7fdb387339609ad877

Fix list continuations with +
Broken by #36, since a line with a plus sign can also be a header
For lack of better solutions, setext headers should have at least two
characters (even if not really required by asciidoc) to be properly
highlighted.

syntax/asciidoctor.vim
@@ -53,7 +53,7 @@ syn region asciidoctorH4 start="^=====\s" end="$" oneline contains=@asciidoctorI
 syn region asciidoctorH5 start="^======\s" end="$" oneline contains=@asciidoctorInline,@Spell
 syn region asciidoctorH6 start="^=======\s" end="$" oneline contains=@asciidoctorInline,@Spell
 
-syn match asciidoctorSetextHeader '^\%(\n\|\%^\)\k.*\n\%(=\+\|\-\+\|\~\+\|\^\+\|+\+\)$'   contains=@Spell
+syn match asciidoctorSetextHeader '^\%(\n\|\%^\)\k.*\n\%(==\+\|\-\-\+\|\~\~\+\|\^\^\+\|++\+\)$'   contains=@Spell
 
 syn sync clear
 syn sync match syncH1 grouphere NONE "^==\s.*$"